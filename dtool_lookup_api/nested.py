622e1d63c9179973bd740076414a1795977936ac
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2021-06-16 10:14:32 +0200

    ENH: submodule working in both notebooks and python files.

---
+++ dtool_lookup_api/nested.py
@@ -0,0 +1,22 @@
+import asyncio
+import dtool_lookup_api.asynchronous as dl
+
+if asyncio.get_event_loop().is_running():
+    # then we are in jupyter notebook
+    # this allows nested event loops, i.e. calls to asyncio.run inside the notebook as well
+    # This way, the same code works in notebook and python
+    import nest_asyncio
+
+    nest_asyncio.apply()
+
+
+def wrap_asyncio(fun):
+    def wrapped(*args, **kwargs):
+        return asyncio.run(fun(*args, **kwargs))
+
+    return wrapped
+
+
+query = wrap_asyncio(dl.query)
+
+# TODO: automate this: https://stackoverflow.com/questions/39184338/how-can-i-decorate-all-functions-imported-from-a-file
