1b0f95e55562d8f2281a4c878eaa0ed0992f44b3
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-12-06 14:02:45 +0100

    ENH: Automatically import methods of ConfigurationBasedLookupClient

--- dtool_lookup_api/asynchronous.py
+++ dtool_lookup_api/asynchronous.py
@@ -1,74 +1,40 @@
+#
+# Copyright 2020 Lars Pastewka, Johannes Laurin Hoermann
+#
+# ### MIT license
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 """dtool_lookup_client.async module."""
+
+import inspect
+
 from .core.LookupClient import ConfigurationBasedLookupClient
 
+# Import all methods from ConfigurationBasedLookupClient into the global namespace
+for name, func in inspect.getmembers(ConfigurationBasedLookupClient, predicate=inspect.isfunction):
+    async def _wrap_client(*args, **kwargs):
+        async with ConfigurationBasedLookupClient() as lookup_client:
+            return await getattr(lookup_client, name)(*args, **kwargs)
+        __doc__ = func.__doc__
 
-# TODO: instead of wrapping every method of LookupClient explicitly, automize
-
-async def all():
-    """Wraps around LookupClient method 'all'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.all()
-
-async def aggregate(aggregation):
-    """Wraps around LookupClient method 'aggregate'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.aggregate(aggregation)
-
-async def config():
-    """Wraps around LookupClient method 'config'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.config()
-
-
-async def lookup(uuid):
-    """Wraps around LookupClient method 'lookup'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.lookup(uuid)
-
-
-async def manifest(uri):
-    """Wraps around LookupClient method 'query'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.manifest(uri)
-
-
-async def query(query):
-    """Wraps around LookupClient method 'query'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.query(query)
-
-
-async def readme(uri):
-    """Wraps around LookupClient method 'readme'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.readme(uri)
-
-
-async def search(keyword=None):
-    """Wraps around LookupClient method 'search'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.search(keyword)
-
-
-async def list_users():
-    """Wraps around LookupClient method 'list_users'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.list_users()
-
-
-async def register_user(username, is_admin=False):
-    """Wraps around LookupClient method 'list_users'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.register_user(username, is_admin)
-
-
-async def permission_info(base_uri):
-    """Wraps around LookupClient method 'list_users'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.permission_info(base_uri)
-
-
-async def update_permissions(base_uri, users_with_search_permissions, users_with_register_permissions=[]):
-    """Wraps around LookupClient method 'list_users'."""
-    async with ConfigurationBasedLookupClient() as lookup_client:
-        return await lookup_client.update_permissions(base_uri, users_with_search_permissions, users_with_register_permissions)
+    # Import everything that does not start with an underscore
+    if not name.startswith('_'):
+        globals()[name] = _wrap_client
