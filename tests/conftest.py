7d944406bbf7252aa42d3475bde34d1d4e0ebfcf
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-10-17 10:18:44 +0200

    MAINT: Undo accidential change of testing ports

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,8 @@ import pytest
 from environ import TemporaryOSEnviron
 
 DTOOL_LOOKUP_SERVER_ADDRESS_AND_CREDENTIALS = {
-    "DTOOL_LOOKUP_SERVER_URL": "https://localhost:5100",
-    "DTOOL_LOOKUP_SERVER_TOKEN_GENERATOR_URL": "https://localhost:5101/token",
+    "DTOOL_LOOKUP_SERVER_URL": "https://localhost:5000",
+    "DTOOL_LOOKUP_SERVER_TOKEN_GENERATOR_URL": "https://localhost:5001/token",
     "DTOOL_LOOKUP_SERVER_USERNAME": "testuser",
     "DTOOL_LOOKUP_SERVER_PASSWORD": "test_password",
     "DTOOL_LOOKUP_SERVER_VERIFY_SSL": False,
